700163b2907a005f7fd2e96cdb032ff9ff6c389b
Ademheyar <ademheyar@gmail.com>
2026-01-22 13:55:02 -0500

    trying

--- D/Calan.py
+++ D/Calan.py
@@ -10,6 +10,7 @@ from D.ItemSelector import ItemSelectorWidget
 import os
 
 
+
 data_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'data'))
 db_path = os.path.join(data_dir, 'my_database.db')
 conn = sqlite3.connect(db_path)
